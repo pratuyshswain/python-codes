e08cc977d0db56f924ae09c9c4790789a2870295
Pratyush Swain <pratuyshswain884@gmail.com>
2026-08-03 16:13:45 +0530

    add func.py

---
+++ func.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+def isGreater(a, b):#here def is built in and isGreater is userdefined
+    if a > b:
+        print(a,"is greater than ",b)
+    else:
+        print(b,"is greater than ",a)
+isGreater(a,b)
+#if no argument is under the function
+def islesser(a, b):
+    pass                #so it skips the function
+
+#ex
+def greet_user(name,age):
+    print("Hello, " + name + "!")
+    print(age, "years old!")
+greet_user("Alice",20)
+#
+def greet_user(c,d):
+    print("Hello, " + c + "!")
+    print(d, "years old!")

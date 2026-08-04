6492becc4a0d211cb428cb4280bb9d1d514566e3
Pratyush Swain <pratuyshswain884@gmail.com>
2026-08-04 23:51:49 +0530

    update

--- func.py
+++ func.py
@@ -65,3 +65,24 @@ print(
     squared_call(mult_by_five, 1),
     sep='\n',
 )# '\n' is the newline character - it starts a new line
+# max function
+
+def mod_5(x):
+    """Return the remainder of x after dividing by 5"""
+    return x % 5
+
+print(
+    'Which number is biggest?',
+    max(100, 51, 14),
+    'Which number is the biggest modulo 5?',
+    max(100, 51, 14, key=mod_5),
+    sep='\n',
+)
+#round function
+def rounded_numbers(num):
+    return round(num)
+print(rounded_numbers(1.123456))
+# with two decimal number right shift
+def rounded_numbers(num):
+    return round(num,2)
+print(rounded_numbers(1.123456))

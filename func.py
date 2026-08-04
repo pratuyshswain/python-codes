b3b73447ae36bb4160149faaa7b74799a664e294
Pratyush Swain <pratuyshswain884@gmail.com>
2026-08-04 22:39:51 +0530

    update func.py

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+#def function
 a=int(input("Enter the first number: "))
 b=int(input("Enter the second number: "))
 def isGreater(a, b):#here def is built in and isGreater is userdefined
@@ -35,3 +36,32 @@ elif number==0:
     print(number)
 else:
     print(abs(number))
+#min function
+def least_difference(a, b, c):
+    diff1 = abs(a - b)
+    diff2 = abs(b - c)
+    diff3 = abs(a - c)
+    return min(diff1, diff2, diff3)     #print the min value among the diff1,2,3
+print(                                          # calling part
+    least_difference(1, 10, 100),
+    least_difference(1, 10, 10),
+    least_difference(5, 6, 7), # Python allows trailing commas in argument lists .
+)
+#Functions Applied to Functions
+
+def mult_by_five(x):
+    return 5 * x
+
+def call(fn, arg):
+    """Call fn on arg"""
+    return fn(arg)                              #mean mult_by_five(5)
+
+def squared_call(fn, arg):
+    """Call fn on the result of calling fn on arg"""
+    return fn(fn(arg))                                              #mean mult_by_five(mult_by_five(5))
+
+print(
+    call(mult_by_five, 1),
+    squared_call(mult_by_five, 1),
+    sep='\n',
+)# '\n' is the newline character - it starts a new line

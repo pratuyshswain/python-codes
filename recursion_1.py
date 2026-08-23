a2d69b79860dd2d30f2d897cfd8559c52c9f5f06
Pratyush Swain <pratuyshswain884@gmail.com>
2026-08-23 11:50:27 +0530

    new

--- recursion_1.py
+++ recursion_1.py
@@ -2,35 +2,30 @@
 #factorial(4)=4*factorial(3) return
 #factorial(3)=3*factorial(2) return
 #factorial(2)=2*factorial(1) return
-# def factorial(n):
-#     if n == 0 or n == 1:
-#         return 1
-#     else:
-#         return n * factorial(n-1)
-# print(factorial(5))
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(5))
 #ex-2
-# def countdown(count):
-#     # 1. The Base Case (The Stopping Point)
-#     if count == 0:
-#         print("Liftoff! 🚀")
-#         return  # This tells the function to completely stop
-#
-#     # 2. The Recursive Case (The Work)
-#     print(count)
-#     countdown(count - 1)  # The function calls ITSELF, but with a smaller
-# print(countdown(7))
+def countdown(count):
+    # 1. The Base Case (The Stopping Point)
+    if count == 0:
+        print("Liftoff! 🚀")
+        return  # This tells the function to completely stop
 
-# # f(0)=o                          =0
-# # f(1)=1                          =1
-# # f(2)=f(1)+f(0)                  =1
-# # f(3)=f(2)+f(1)                  =2
-# # f(4)=f(3)+f(2)                  =3
-# # f(n)=f(n-1)+f(n-2)
-# num=0
-# while num<10:
-#     num=num+1
-#     print(num)
-    # num =num+1
+    # 2. The Recursive Case (The Work)
+    print(count)
+    countdown(count - 1)  # The function calls ITSELF, but with a smaller
+print(countdown(7))
+
+# f(0)=o                          =0
+# f(1)=1                          =1
+# f(2)=f(1)+f(0)                  =1
+# f(3)=f(2)+f(1)                  =2
+# f(4)=f(3)+f(2)                  =3
+# f(n)=f(n-1)+f(n-2)
 def fibonacci(n):
     if n == 0:
         return 0

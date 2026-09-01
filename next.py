044f488f3c8de416fb9e1d0cae99444c3e5f1912
Pratyush Swain <pratuyshswain884@gmail.com>
2026-09-01 23:24:26 +0530

    update

--- next.py
+++ next.py
@@ -51,4 +51,12 @@ print(snack)
 animal=["cow","tiger","monkey","dog","cat"]
 animal.clear()
 print(animal)
+x = 12
+# x is a real number, so its imaginary part is 0.
+print(x.imag)
+# Here's how to make a complex number, in case you've ever been curious:
+c = 12 + 3j
+print(c.imag)
+x = 12
+print(x.bit_length())
 
